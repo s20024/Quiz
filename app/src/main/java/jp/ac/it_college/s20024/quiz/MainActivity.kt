86894a1e3da49e7c1e768e53ea6e5099e0ff3692
s20024 <s20024@std.it-college.ac.jp>
2021-08-18 16:13:48 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/it_college/s20024/quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.s20024.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
