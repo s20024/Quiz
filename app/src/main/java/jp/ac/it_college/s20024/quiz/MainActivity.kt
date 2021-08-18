17435db5893e3feecf0bab192d9f130e10764707
s20024 <s20024@std.it-college.ac.jp>
2021-08-18 16:26:12 +0900

    test

--- app/src/main/java/jp/ac/it_college/s20024/quiz/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/s20024/quiz/MainActivity.kt
@@ -2,10 +2,15 @@ package jp.ac.it_college.s20024.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.s20024.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        println("unnkoooooooooooooooooooooo")
     }
 }
